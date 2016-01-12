645f256407c9b1558e23423f3a23ebc5d3475fde
Z_小圣 <1278105489@qq.com>
2016-01-12 19:20:38 +0800

    Initial Commit

---
+++ ZXSWeather/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ZXSWeather
+//
+//  Created by Z_小圣 on 16/1/12.
+//  Copyright © 2016年 Z_小圣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ZXSWeather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZXSWeather
+//
+//  Created by Z_小圣 on 16/1/12.
+//  Copyright © 2016年 Z_小圣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
